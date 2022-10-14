d8b812e88ffe37967b3c79af9657fb4b5df8840a
MiguelBre <migbress12@gmail.com>
2022-10-14 11:23:59 -0300

    14/10/2022

--- app/src/main/java/br/senai/sp/jandira/gameproject/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/gameproject/MainActivity.kt
@@ -1,11 +1,29 @@
 package br.senai.sp.jandira.gameproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import br.senai.sp.jandira.gameproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        binding.textSignup.setOnClickListener{
+            val openNewAccountActivity = Intent(this, NewAccountActivity::class.java)
+            startActivity(openNewAccountActivity)
+        }
     }
 }
--- app/src/main/java/br/senai/sp/jandira/gameproject/NewAccountActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/gameproject/NewAccountActivity.kt
@@ -2,10 +2,35 @@ package br.senai.sp.jandira.gameproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class NewAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_account)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.menu_save){
+            Toast.makeText(this, "Clicou em salvar", Toast.LENGTH_SHORT).show()
+            return true
+        } else if(item.itemId == R.id.menu_settings){
+            Toast.makeText(this, "Clicou em configureções", Toast.LENGTH_SHORT).show()
+            return true
+        } else {
+            Toast.makeText(this, "clicou em Sair", Toast.LENGTH_SHORT).show()
+            return true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_new_user, menu)
+
+        return true
     }
 }
